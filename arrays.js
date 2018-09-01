57d83cfbb4c6d8277f3082ae6b7ad5cf5e2a82e1
Moritz M <moritz.muehlhausen@gmail.com>
2018-09-01 23:19:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -41,5 +41,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array2.slice(0, array2.length - 1)
+  return array2.slice(0, array2.length - 1)
 }
