98de292fa9a3d40a42e8d68b5c6d8164fc2e210f
Moritz M <moritz.muehlhausen@gmail.com>
2018-09-01 22:36:09 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+var array = [1]
+
+function addElementToBeginningOfArray (array, element) {
+  return ["foo", ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift("foo")
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, "foo"]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push("foo")
+  return array
+}
